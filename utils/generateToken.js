e1bc5973e462c29bd8112cac8ccc5314a6a10aac
Manish <127071839+manish-vm@users.noreply.github.com>
2025-12-11 17:24:41 +0530

    Change cookie settings to secure and sameSite none
    Updated cookie settings for JWT token generation.

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -7,11 +7,11 @@ const generateToken = (res, userId, isAdmin) => {
 
   // httpOnly cookie
   res.cookie('jwt', token, {
-    httpOnly: true,
-    secure: process.env.NODE_ENV === 'production',
-    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
-    maxAge: 7 * 24 * 60 * 60 * 1000
-  });
+  httpOnly: true,
+  secure: true,
+  sameSite: 'none',
+  maxAge: 7 * 24 * 60 * 60 * 1000
+});
 
   return token;
 };
